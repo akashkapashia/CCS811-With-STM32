e10af7a86618e1663d059c11722059cfee96c19d
akash kapashia <akashkapashia@gmail.com>
2017-08-25 13:20:06 +0530

    Update CCS811_Basic.c

--- CCS811_Basic.c
+++ CCS811_Basic.c
@@ -1,45 +1,24 @@
-	/**
-		******************************************************************************
-		* File Name          : CCS811_Basic.c
-		* Description        : Main program body
-		******************************************************************************
-		*
-		* COPYRIGHT(c) 2017 STMicroelectronics
-		*
-		* Redistribution and use in source and binary forms, with or without modification,
-		* are permitted provided that the following conditions are met:
-		*   1. Redistributions of source code must retain the above copyright notice,
-		*      this list of conditions and the following disclaimer.
-		*   2. Redistributions in binary form must reproduce the above copyright notice,
-		*      this list of conditions and the following disclaimer in the documentation
-		*      and/or other materials provided with the distribution.
-		*   3. Neither the name of STMicroelectronics nor the names of its contributors
-		*      may be used to endorse or promote products derived from this software
-		*      without specific prior written permission.
-		*
-		* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-		* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-		* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-		* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-		* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-		* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-		* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-		* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-		* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-		* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-		*
-		******************************************************************************
-		*/
+/**
+	******************************************************************************
+	* File Name          : CCS811.C
+	* Description        : This file contains the common defines of the application
+	******************************************************************************
+	*
+	* COPYRIGHT(c) 2017 Akash kapashia
+       * Created by Akash kapashia
+	******************************************************************************
+*/
+ 
 	/* Includes ------------------------------------------------------------------*/
-	#include "CCS811_Basic.h"
-  #include "HDC_1080_TH.h"
+	 #include "CCS811_Basic.h"
+         #include "HDC_1080_TH.h"
 	 #define I2CXC I2C1
 	 I2C_HandleTypeDef  hi2cxc;
-   uint8_t BurnIn_Time_Complete=0;
+         uint8_t BurnIn_Time_Complete=0;
 	 uint8_t RunIn_Time_Complete=0;
 	 uint8_t Baseline_Time_Complete=0;
-   uint8_t EBaseline_Time_Complete=0;
-	//These are the air quality values obtained from the sensor
+         uint8_t EBaseline_Time_Complete=0;
+  	//These are the air quality values obtained from the sensor
 	 unsigned int tVOC = 0;
 	 unsigned int CO2 = 0;
 	 uint16_t adc_raw=0;
@@ -50,14 +29,16 @@
 	 uint8_t mosetting=0;
 	 uint8_t dtvalue =0;	 
 	 uint8_t appStart=0;
-   uint32_t ELBaseline_period=0;
+         uint32_t ELBaseline_period=0;
 	 uint32_t ALBaseline_period=0;
 	 uint8_t  Mode_CCS811=1;
- float relativeHumidity=65.0,temperature=25.0;
-	extern	uint16_t HDC1080_Temp ;
- 
-extern	uint16_t HDC1080_Humidity ;
+         float relativeHumidity=65.0,temperature=25.0;
 
+/**
+  * @brief  Init_I2C_CCS811.
+  * @param  NONE.
+  * @retval None.
+ */	
  void Init_I2C_CCS811(void)
 {
  
@@ -80,23 +61,24 @@ extern	uint16_t HDC1080_Humidity ;
   if (HAL_I2CEx_ConfigAnalogFilter(&hi2cxc, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
  while(1); 
-	}
+  }
 
     /**Configure Digital filter 
     */
   if (HAL_I2CEx_ConfigDigitalFilter(&hi2cxc, 0) != HAL_OK)
   {
  while(1); 
-	}
-	
+  }
 
 }	
 		
-	//Updates the total voltatile organic compounds (TVOC) in parts per billion (PPB)
-	//and the CO2 value
-	//Returns nothing
-	void readAlgorithmResults()
-	{
+/*
+  * @brief  Updates the total voltatile organic compounds (TVOC) in parts per billion (PPB) and the CO2 value.
+  * @param  NONE.
+  * @retval None.
+ */	
+void readAlgorithmResults()
+{
 	 
 		uint8_t data_rq[4];
 		uint8_t status = HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, ( uint8_t )CSS811_ALG_RESULT_DATA, I2C_MEMADD_SIZE_8BIT, data_rq, 4,
@@ -112,65 +94,62 @@ extern	uint16_t HDC1080_Humidity ;
 		eC02 value, in parts per million (ppm) */
 		CO2 = ((unsigned int)co2MSB << 8) | co2LSB;
 		tVOC = ((unsigned int)tvocMSB << 8) | tvocLSB;
-	}
+}
 
-	//Turns on the sensor and configures it with default settings
-	void configureCCS811()
-	{
-		HAL_Delay(69);
+/*
+  * @brief  configureCCS811.
+  * @param  NONE.
+  * @retval None.
+ */
+void configureCCS811()
+{
+		
+	        HAL_Delay(69);
 		//Verify the hardware ID is what we expect
 		
 		uint8_t hwID = readRegister(0x20); //Hardware ID should be 0x81
-		if (hwID != 0x81)
+	if (hwID != 0x81)
 		{
 			//Serial.println("CCS811 not found. Please check wiring.");
 			while (1); //Freeze!
 		}
 	 
-	 
-		 
-	 
 	
 		
-	 uint8_t    lodata[1];
-	 lodata[0]= CSS811_APP_START;
+	       uint8_t    lodata[1];
+	       lodata[0]= CSS811_APP_START;
 		
-	 HAL_I2C_Master_Transmit(&hi2cxc, 0xB6, lodata, 1, 100);
+	       HAL_I2C_Master_Transmit(&hi2cxc, 0xB6, lodata, 1, 100);
 	 
-		HAL_Delay(10);
-
-		
-			HAL_Delay(10);
+		HAL_Delay(20);
 		setDriveMode(Mode_CCS811); //Read every second
-    HAL_Delay(10);	
+                HAL_Delay(10);	
 		
 	 if (checkForError() == 1)
 		{
-	 
-			while (1); //Freeze!
+	        while (1); //Freeze!
 		}
 		//Set Drive Mode
 	
 		//Check for errors
 		if (checkForError() == 1)
 		{
-	 
-			while (1); //Freeze!
+		while (1); //Freeze!
 		}
 		readHumidity();
-    readTemperature();
+                readTemperature();
 		setEnvironmentalData(  HDC1080_Humidity/1000,  HDC1080_Temp);
-    //RunIn_Time_Complete= BURN_IN_TIME_ADDRESS;
-    BurnIn_Time_Complete=BURN_IN_TIME_ADDRESS_VAL ;
+                //RunIn_Time_Complete= BURN_IN_TIME_ADDRESS;
+                BurnIn_Time_Complete=BURN_IN_TIME_ADDRESS_VAL ;
 		Baseline_Time_Complete=BASELINE_EARLYLIFE_PERIOD_ADDRESS_VAL;
 		
-	}
+}
 
 	//Checks to see if error bit is set
 	FlagStatus checkForError()
 	{
 		errvalue=readRegister(CSS811_ERROR_ID);
-			errvalue = readRegister(CSS811_STATUS);
+	        errvalue = readRegister(CSS811_STATUS);
 		return (errvalue & 1 << 0);
 	}
 
@@ -179,15 +158,15 @@ extern	uint16_t HDC1080_Humidity ;
 	//Checks to see if DATA_READ flag is set in the status register
 	FlagStatus dataAvailable()
 	{   // dtvalue = readRegister(CSS811_ERROR_ID);
-//HAL_Delay(00);
-			dtvalue = readRegister(CSS811_STATUS);
+                 //HAL_Delay(00);
+	        dtvalue = readRegister(CSS811_STATUS);
 		return (dtvalue & 1 << 3);
 	}
 
 	//Checks to see if APP_VALID flag is set in the status register
 	FlagStatus appValid()
 	{
-			appvalue = readRegister(CSS811_STATUS);
+		appvalue = readRegister(CSS811_STATUS);
 		return (appvalue & (1 << 4));
 	}
 
@@ -207,29 +186,39 @@ extern	uint16_t HDC1080_Humidity ;
 		writeRegister(CSS811_MEAS_MODE, setting);
 	}
 
-	//Mode 0 = Idle
-	//Mode 1 = read every 1s
-	//Mode 2 = every 10s
-	//Mode 3 = every 60s
-	//Mode 4 = RAW mode
-	void setDriveMode(uint8_t mode)
-	{
+/*
+  * @brief  //Mode 0 = Idle
+	    //Mode 1 = read every 1s
+	    //Mode 2 = every 10s
+	    //Mode 3 = every 60s
+	    //Mode 4 = RAW mode.
+  * @param  MODE.
+  * @retval None.
+ */
+void setDriveMode(uint8_t mode)
+{
 		if (mode > 4) mode = 4; //Error correction
 	 
-			mosetting = readRegister(CSS811_MEAS_MODE); //Read what's currently there
+		mosetting = readRegister(CSS811_MEAS_MODE); //Read what's currently there
 	 
 		mosetting &=~(7<<4); //Clear DRIVE_MODE bits
 		mosetting |= (mode << 4); //Mask in mode
 	 
 		writeRegister(CSS811_MEAS_MODE, mosetting);
-			mosetting = readRegister(CSS811_MEAS_MODE); //Read what's currently there
+		mosetting = readRegister(CSS811_MEAS_MODE); //Read what's currently there
 	 
-	}
+}
 
-	//Given a temp and humidity, write this data to the CSS811 for better compensation
-	//This function expects the humidity and temp to come in as floats
-	void setEnvironmentalData(float relativeHumidity, float temperature)
-	{
+ 
+ /*
+  * @brief //Given a temp and humidity, write this data to the CSS811 for better compensation
+	 //This function expects the humidity and temp to come in as floats
+  * @param  relativeHumidity HUMIDITY.
+  * @param  temperature TEMPERATURE.
+  * @retval None.
+ */
+void setEnvironmentalData(float relativeHumidity, float temperature)
+{
 		int rH = relativeHumidity * 1000; //42.348 becomes 42348
 		int temp = temperature * 1000; //23.2 becomes 23200
 
@@ -258,7 +247,7 @@ extern	uint16_t HDC1080_Humidity ;
 		env[2]=envData[1];
 		env[3]=envData[2];
 		env[5]=envData[3];
-			HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, ( uint8_t )CSS811_ENV_DATA, I2C_MEMADD_SIZE_8BIT, envData, 4,
+		HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, ( uint8_t )CSS811_ENV_DATA, I2C_MEMADD_SIZE_8BIT, envData, 4,
 																100 );
 		 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
 			{
@@ -269,33 +258,48 @@ extern	uint16_t HDC1080_Humidity ;
 			}
 		 
 	}
+
+
+ /*
+  * @brief  reading_Multiple_Register
+  * @param  addr ADDRESS.
+  * @param  val VALUE.
+  * @param  size SIZE.
+  * @retval None.
+ */
 void read_Mul_Register(uint8_t addr, uint8_t * val,uint8_t size)
 	{
-			HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, ( uint8_t )addr, I2C_MEMADD_SIZE_8BIT, val, size,
-																100 );
+			HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, ( uint8_t )addr, I2C_MEMADD_SIZE_8BIT, val, size,100 );
 		
 	}
-	
+
+ /*
+  * @brief  softRest
+  * @param  NONE.
+  * @retval None.
+ */
 void softRest() {
      
-    uint8_t rstCMD[5] = {CSS811_SW_RESET, 0x11,0xE5,0x72,0x8A};
+               uint8_t rstCMD[5] = {CSS811_SW_RESET, 0x11,0xE5,0x72,0x8A};
  
-     	HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, CSS811_SW_RESET, I2C_MEMADD_SIZE_8BIT, rstCMD, 5,
-																300);
-		 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
-			{
-			} 
-      
+         	HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, CSS811_SW_RESET, I2C_MEMADD_SIZE_8BIT, rstCMD, 5,300);
+		while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY);
+	      
 }	
 
+
+ /*
+  * @brief  get_Sensor_Resistance
+  * @param  NONE.
+  * @retval RESISTANCE OF SENSOR.
+ */
 uint32_t get_Sensor_Resistance(){
 	uint8_t sensor_Resistance_raw[2];
 
 	uint32_t sensor_Resistance;
 	
-	HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, CSS811_RAW_DATA , I2C_MEMADD_SIZE_8BIT, sensor_Resistance_raw, 2,
-																100 );
-  current_value=sensor_Resistance_raw[0]>>2;
+	HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, CSS811_RAW_DATA , I2C_MEMADD_SIZE_8BIT, sensor_Resistance_raw, 2,100 );
+        current_value=sensor_Resistance_raw[0]>>2;
 	sensor_Resistance_raw[0]=sensor_Resistance_raw[0]&0x03;
 	
 	adc_raw=(sensor_Resistance_raw[0]<<8)|sensor_Resistance_raw[1];
@@ -305,65 +309,77 @@ uint32_t get_Sensor_Resistance(){
 	return sensor_Resistance;
 }
 
-
+ /*
+  * @brief  sleep
+  * @param  NONE.
+  * @retval NONE.
+ */
 void sleep()
 {
-  //_digitalWrite(_WAKE_PIN, LOW);
   // sets sensor to idle; measurements are disabled; lowest power mode
  writeRegister(CSS811_MEAS_MODE, 0);
-	
-//  digitalWrite(_WAKE_PIN, HIGH);  // set WAKE_PIN high - this puts sensor in sleep mode (~2uA) and all I2C communications are ignored
-}
+ }
 
 
 
-	//Reads from a give location from the CSS811
-	uint8_t readRegister(uint8_t addr)
-		{
+ /*
+  * @brief  Reads from a give location from the CSS811
+  * @param  addr  ADDRESS.
+  * @retval VALUE AT THE ADDRESS.
+ */	 
+uint8_t readRegister(uint8_t addr)
+{
 			uint8_t dt;
 					
 			 HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, ( uint8_t )addr,1, &dt, 1,
 																	300 );
-				 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
-				{
-				} 
+				 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY);
 		 
 		 
 			return dt;
-		 }
+ }
 		
 		 
 		 
-   uint32_t  restore_Baselines;
-		 //restore the baseline value
-	//Used for telling sensor what 'clean' air is
-	//You must put the sensor in clean air and record this value
-   void restore_Baseline()
+uint32_t  restore_Baselines;
+		 
+/*
+  * @brief  	 //restore the baseline value
+	       //Used for telling sensor what 'clean' air is
+	      //You must put the sensor in clean air and record this value
+  * @param  NONE.
+  * @retval NONE.
+ */
+void restore_Baseline()
   {   
-   uint32_t  restore_Baseline;
+                 uint32_t  restore_Baseline;
 		 restore_Baseline= * ((  uint32_t *)DATA_EEPROM_BASE);
 		
 		restore_Baselines=restore_Baseline ;
 		uint8_t res_bs[2];
 		res_bs[0]=restore_Baseline>>8;
 		res_bs[1]=restore_Baseline&0x000000FF;
-    	HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, CSS811_BASELINE, I2C_MEMADD_SIZE_8BIT, res_bs,2,
-																300);
+    	        HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, CSS811_BASELINE, I2C_MEMADD_SIZE_8BIT, res_bs,2,300);
 		 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
 			{
 			} 
     }
-	//Returns the baseline value and saves into EEPROM
-	//Used for telling sensor what 'clean' air is
-	//You must put the sensor in clean air and record this value
-	unsigned int getBaseline()
-	{
+
+ 
+/*
+  * @brief  	//Returns the baseline value and saves into EEPROM
+	       //Used for telling sensor what 'clean' air is
+	      //You must put the sensor in clean air and record this value
+  * @param  NONE.
+  * @retval BASELINE VALUE.
+ */
+unsigned int getBaseline()
+{
 		 
-  	uint8_t ada[2];
+  	        uint8_t ada[2];
 		HAL_StatusTypeDef status = HAL_OK;
 	 
-		  status = HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, ( uint8_t )CSS811_BASELINE,1,  ada, 2,
-																100 );
+		  status = HAL_I2C_Mem_Read( &hi2cxc, CCS811_ADDRD, ( uint8_t )CSS811_BASELINE,1,  ada, 2,100 );
 			 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
 			{
 			} 
@@ -377,23 +393,25 @@ void sleep()
 
 		unsigned int baseline = ((unsigned int)baselineMSB << 8) | baselineLSB;
 		HAL_FLASHEx_DATAEEPROM_Unlock();
-	  HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_WORD, DATA_EEPROM_BASE, baseline);
-			
-			
-			
+	        HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_WORD, DATA_EEPROM_BASE, baseline);
 			
 		return (baseline);
-	}
+}
 
 
 
 	//Write a value to a spot in the CCS811
-	void writeRegister(uint8_t addr, uint8_t val)
-	{
+/*
+  * @brief  Write a value to a spot in the CCS811
+  * @param  addr ADDRESS.
+  * @param  val  VALUE.
+  * @retval NONE.
+ */
+void writeRegister(uint8_t addr, uint8_t val)
+{
 			
 
-			HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, ( uint8_t )addr, I2C_MEMADD_SIZE_8BIT, &val, 1,
-																300);
+			HAL_I2C_Mem_Write( &hi2cxc, CCS811_ADDWR, ( uint8_t )addr, I2C_MEMADD_SIZE_8BIT, &val, 1,300);
 		 while (HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
 			{
 			} 
@@ -401,7 +419,7 @@ void sleep()
 			while(HAL_I2C_GetState(&hi2cxc) != HAL_I2C_STATE_READY)
 			{
 			}
-	}
+}
 	 
 
 
